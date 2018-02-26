6b819456911c48cc0ddaf27f1946f7cbf0323783
JohnJeong123 <frenchofkiss@gmail.com>
2018-02-26 14:37:05 +0900

    added gitignore

---
+++ prjKakaoExample.dpr
@@ -0,0 +1,13 @@
+program prjKakaoExample;
+
+uses
+  Forms,
+  Example in 'Example.pas' {Form1};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
